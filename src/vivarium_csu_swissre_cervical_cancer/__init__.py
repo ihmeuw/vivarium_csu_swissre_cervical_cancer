bae1e1cfd6e19f5023e18f5f995a734432a9a6d5
Matthew Kappel <mkappel@uw.edu>
2020-09-09 14:56:47 -0700

    "Template commit"

---
+++ src/vivarium_csu_swissre_cervical_cancer/__init__.py
@@ -0,0 +1,5 @@
+"""vivarium_csu_swissre_cervical_cancer
+
+Research repository for the vivarium_csu_swissre_cervical_cancer project.
+
+"""
---
+++ src/vivarium_csu_swissre_cervical_cancer/paths.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+import vivarium_csu_swissre_cervical_cancer.globals as project_globals
+
+BASE_DIR = Path(vivarium_csu_swissre_cervical_cancer.__file__).resolve().parent
+
+ARTIFACT_ROOT = Path(f"/share/costeffectiveness/artifacts/{project_globals.PROJECT_NAME}/")
+MODEL_SPEC_DIR = BASE_DIR / 'model_specifications'
+RESULTS_ROOT = Path(f'/share/costeffectiveness/results/{project_globals.PROJECT_NAME}/')
